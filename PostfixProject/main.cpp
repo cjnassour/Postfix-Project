8cb8831727704796f0d3a4e02b91e2d87908ab46
cjnassour <cjnassour@gmail.com>
2015-09-14 14:07:25 -0700

    Update main.cpp

--- PostfixProject/main.cpp
+++ PostfixProject/main.cpp
@@ -6,10 +6,10 @@ using namespace std;
 int main(){
 	system("color 1A");
 
-	vector<string> myvar;		//this vector will store the variables the user enters
+	vector<string> myvar;			//this vector will store the variables the user enters
 	char inputs[15];			//this array will store the user's initial input
 
-	cout << "Enter postfix: ";	//get user postfix input
+	cout << "Enter postfix: ";		//get user postfix input
 	cin >> inputs;			
 	string vecnumber;			//used for converting char to string
 
@@ -30,11 +30,11 @@ int main(){
 
 	cout << endl;
 
-	vector<int> answer;	//this vector will be used to perform the operations, and store the answer
+	vector<int> answer;			//this vector will be used to perform the operations, and store the answer
 
 	for (int i = 0; i < myvar.size(); i++)
 	{
-		if (inputs[i] >= 'A' && inputs[i] <= 'z')	 
+		if (inputs[i] >= 'A' && inputs[i] <= 'z')	//variable	 
 			{
 				int x = stoi(myvar[i]);			//convert string to int
 				answer.push_back(x);			//add number to vector for calculation
@@ -44,20 +44,20 @@ int main(){
 			{
 				int sum = answer.end()[-1] + answer.end()[-2];	//last + 2nd to last element
 				answer.pop_back();								//delete last element
-				answer.end()[-1] = sum;							//assign last element to product
+				answer.end()[-1] = sum;								//assign last element to product
 			}
 
 		else if (inputs[i] == '*')	//multiplication
 			{ 
 				int product = answer.back() * answer.end()[-2];	//last * 2nd to last element
 				answer.pop_back();								//delete last element
-				answer.end()[-1] = product;						//assign last element to product
+				answer.end()[-1] = product;							//assign last element to product
 			}
 		else if (inputs[i] == '-')	//subtraction
 			{ 
 				int diff = answer.back() - answer.end()[-2];	//last - 2nd to last element
 				answer.pop_back();								//delete last element
-				answer.end()[-1] = diff;						//assign last element to product
+				answer.end()[-1] = diff;							//assign last element to product
 			} 
 
 	}
